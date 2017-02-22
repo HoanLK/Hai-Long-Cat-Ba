f466be616a0ed92472d259eddbdfe156013790ad
llathanh1108 <thanh1108@gmail.com>
2017-02-22 15:38:52 +0700

    Init

---
+++ HaiLongHotel/HaiLongHotel/HaiLongHotel/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HaiLongHotel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "";
+
+            return View();
+        }
+        public ActionResult PhongNghi()
+        {
+            return View();
+        }
+        public ActionResult NhaHangSuKien()
+        {
+            return View();
+        }
+        public ActionResult DichVu()
+        {
+            return View();
+        }
+        public ActionResult BaiViet()
+        {
+            return View();
+        }
+    }
+}
