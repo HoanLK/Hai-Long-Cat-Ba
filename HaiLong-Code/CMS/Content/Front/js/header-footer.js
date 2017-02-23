5b1b421bec8598b666cf24cc3e6548944e515a06
llathanh1108 <thanh1108@gmail.com>
2017-02-23 14:58:05 +0700

    thêm sđt vào menu mobile

--- HaiLong-Code/CMS/Content/Front/js/header-footer.js
+++ HaiLong-Code/CMS/Content/Front/js/header-footer.js
@@ -4,8 +4,10 @@ $("#button-search").click(function () {
     if (!$("#search-box").is(":visible")) {
         $("#search-box").slideToggle("medium");
         $("#dl-menu").css("border-bottom", "none");
+        $("#dl-menu").css("transition", "all .3s ease-in-out");
     } else {
         $("#search-box").slideToggle("medium");
-        $("#dl-menu").css("border-bottom", "solid 2px rgb(61, 90, 254)");
+        $("#dl-menu").css("border-bottom", "solid 2px rgb(75, 193, 240)");
+        $("#dl-menu").css("transition", "all .3s ease-in-out");
     }
 });
